f1972f42e10c72fa7ae6e24fe79e5220ad780960
Pedro Bossle <pedro.bossle.s@gmail.com>
2025-05-23 11:30:50 -0300

    Delete styles directory

--- styles/JS/faq.js
+++
@@ -1,9 +0,0 @@
-var elementosDuvida = document.querySelectorAll('.duvida')
-/*document é objeto que armazena varios valores. query é a função que seleciona os elementos da pag que nem no CSS e os bloquinhos*/
-
-elementosDuvida.forEach(function (duvida) {
-    duvida.addEventListener('click', function () {
-        duvida.classList.toggle('ativa')
-    })
-
-})
